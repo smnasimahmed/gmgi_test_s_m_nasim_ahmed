5b265e0568c07d8dbaad01079bdd5358b08757ed
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-05-10 01:25:20 +0600

    clean seperate writing

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:test_exam_1/home.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
